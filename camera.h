c7572c36481e94478271685a179f0f43b55512ae
NovaBankz <45081335+NovaBankz@users.noreply.github.com>
2019-09-28 23:52:32 +0700

    add camera position, vfov to camera.h , and ch10.cpp

--- camera.h
+++ camera.h
@@ -14,9 +14,26 @@ class camera
             lower_left_corner = vec3(-2.0, -1.0, -1.0);
         }
 
-        ray get_ray(float u, float v) const
+        // problem is hereeeeee
+        camera(const vec3 &lookfrom,const vec3 &lookat,const vec3 &vup,float vfov, float aspect)
         {
-            return ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            vec3 u, v, w;
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
+            origin = lookfrom;
+            
+            float theta = vfov * M_PI / 180;
+            float half_height = tan(theta / 2);
+            float half_width = aspect * half_height;
+            lower_left_corner = origin - half_height*v - half_width*u - w;
+            vertical = 2 * half_height * v;
+            horizontal = 2 * half_width * u;
+        }
+
+        ray get_ray(float s, float t) const
+        {
+            return ray(origin, lower_left_corner + s * horizontal + t * vertical - origin);
         }
 
         vec3 origin;
